4cb4df1d132c1ee74088231dba8c71701437a1fc
CristianSalda <118507719+CristianSalda@users.noreply.github.com>
2025-04-24 12:46:33 -0500

    push

--- app/src/main/java/com/apps/qta_24abril/LoginActivity.kt
+++ app/src/main/java/com/apps/qta_24abril/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.apps.qta_24abril
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -14,6 +17,24 @@ class LoginActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar_login)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        toolbar.setNavigationOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        val btnIngresar = findViewById<Button>(R.id.btn_ingresar)
+        val etUsuario = findViewById<EditText>(R.id.et_usuario)
+
+        btnIngresar.setOnClickListener {
+            val textoUsuario = etUsuario.text.toString().trim() // Obtiene el texto ASÍ COMO ESTÉ
+
+            val intent = Intent(this, QuitarFragmentoActivity::class.java).apply {
+                putExtra("TEXTO_USUARIO", textoUsuario) // Envía el texto crudo
+            }
+            startActivity(intent)
+        }
 
 
         toolbar.setNavigationOnClickListener {
---
+++ app/src/main/java/com/apps/qta_24abril/QuitarFragmentoActivity.kt
@@ -0,0 +1,28 @@
+package com.apps.qta_24abril
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class QuitarFragmentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quitar_fragmento)
+
+        val textoRecibido = intent.getStringExtra("TEXTO_USUARIO") ?: "" // Si no hay texto, muestra vacío
+
+        findViewById<TextView>(R.id.tv_bienvenida).text = "Hola $textoRecibido" // Muestra el texto directamente
+
+
+
+
+        // Listeners para los items de navegación
+        findViewById<LinearLayout>(R.id.nav_mitades).setOnClickListener {
+            //startActivity(Intent(this, MitadesActivity::class.java))
+        }
+
+    }
+}
